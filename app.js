b2a24f82677ecfcf8755d6914457c4503deda974
ne-mpfranco <mariapaula.franco@j2.com>
2021-12-10 14:21:05 -0500

    no listn in app

--- app.js
+++ app.js
@@ -85,7 +85,9 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PORT || 5000;
+/* const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
-});
+}); */
+
+module.exports = app;
