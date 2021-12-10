bb01e54a753a115ba08033d2e14efc72e5bd3d4b
ne-mpfranco <mariapaula.franco@j2.com>
2021-12-10 11:45:28 -0500

    aaa

--- app.js
+++ app.js
@@ -82,8 +82,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const PORT = process.env.PORT || 5001;
-
-app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
 
 module.exports = app;
